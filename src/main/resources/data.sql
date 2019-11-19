bc6e91e5b627e4fc3c6ea6886b822da732f16f98
booobi <qqwweee59@gmail.com>
2019-11-19 17:42:31 +0200

    Add startup script with mock data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO EMPLOYEE VALUES
+                        ('1', 23, 'Kiril', 'M', 'Ivanov'),
+                        ('2', 30, 'Simona', 'F', 'Antonova');
+
+
+INSERT INTO BUILDING VALUES
+                        ('1','Aubrey Ashton Road No.3', 2, 23.33, 23.33, 3, '1'),
+                        ('2','Kingsley Nich 25', 3, 23.33, 23.33, 3, null),
+                        ('3','Shar Mountain 77', 4, 23.33, 23.33, 3, '2');
+
+
