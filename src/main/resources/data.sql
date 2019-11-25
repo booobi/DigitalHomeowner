b8eb38efceda3cb2d30a2f4d67644583b29d7f22
booobi <qqwweee59@gmail.com>
2019-11-25 14:26:23 +0200

    Add inhabitants to startup sql script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,4 +8,14 @@ INSERT INTO BUILDING VALUES
                         ('2', 'Kingsley Nich 25', 250, 3, 23.33, 23.33, 3, null),
                         ('3', 'Shar Mountain 77', 725.50, 4, 23.33, 23.33, 3, '2');
 
+INSERT INTO INHABITANT VALUES
+                        ('1', 'Kiril Hristov', '1'),
+                        ('2', 'Ivan Petrov', '1'),
+                        ('3', 'Ivo Andonov', '1'),
+                        ('4', 'Petar Delchev', '1'),
 
+                        ('5', 'Maya Simpova', '2'),
+                        ('6', 'Teodor Manchev', '2'),
+                        ('7', 'Veselin Genchev', '2'),
+
+                        ('8', 'Stefan Popov', '3')
